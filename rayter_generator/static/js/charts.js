2af4289df8832db28203bc4250de133a1b5c6df4
Peter Jaric <peter@jaric.org>
2023-07-08 18:20:57 +0200

    Highlight current line in chart. Add "toggle all lines" button. Refactor. Closes #8.

--- rayter_generator/static/js/charts.js
+++ rayter_generator/static/js/charts.js
@@ -39,27 +39,44 @@ function color(context) {
 }
 
 function setupChart(ctx, gameData) {
-  var myLineChart = new Chart(ctx, {
+  const DEFAULT_LINE_WIDTH = 3;
+
+  let chart = new Chart(ctx, {
     type: "line",
     data: {
-      datasets: Object.keys(gameData.rating_history).map(function(name, index) {
-        let history = gameData.rating_history[name];
-        let theColor = color({ dataIndex: index });
+      datasets: Object.keys(gameData.rating_history)
+        // Sort datasets to make legends come in alphabetical order
+        .sort()
+        .map(function(name, index) {
+          let history = gameData.rating_history[name];
+          let theColor = color({ dataIndex: index });
 
-        return {
-          lineTension: 0.2,
-          fill: false,
-          backgroundColor: theColor,
-          borderColor: theColor,
-          label: name,
-          data: Object.keys(history).map(function(matchNumber) {
-            return { x: matchNumber, y: Math.round(history[matchNumber]) };
-          })
-        };
-      })
+          return {
+            lineTension: 0.2,
+            fill: false,
+            backgroundColor: theColor,
+            borderColor: theColor,
+            borderWidth: DEFAULT_LINE_WIDTH,
+            label: name,
+            data: Object.keys(history).map(function(matchNumber) {
+              return { x: matchNumber, y: Math.round(history[matchNumber]) };
+           })
+          };
+        }
+      )
     },
 
     options: {
+      // Use monotone because it looks a little better
+      // See https://www.chartjs.org/docs/latest/charts/line.html#cubicinterpolationmode
+      cubicInterpolationMode: "monotone",
+      elements: {
+        point: {
+          // Make the points a smaller than default and bigger when hovering
+          radius: 2,
+          hoverRadius: 10
+        }
+      },
       legend: {
         display: true,
         position: "bottom"
@@ -68,15 +85,86 @@ function setupChart(ctx, gameData) {
         mode: "nearest",
         intersect: false
       },
-      scales: {
-        xAxes: [
-          {
-            type: "linear",
-            position: "bottom"
+      plugins: {
+        legend: {
+          title: {
+            display: true,
+            padding: 10,
+            text: "Hover name to highlight line",
+          },
+          position: "bottom",
+          // Make current line stand out when hovering over its legend
+          onHover: (evt, legendItem) => {
+            if (chart.isDatasetVisible(legendItem.datasetIndex)) {
+              let datasets = evt.chart.data.datasets;
+              for (let i = 0; i < datasets.length; i++) {
+                datasets[i].borderWidth = 1;
+              }
+              let data = datasets[legendItem.datasetIndex];
+              data.borderWidth = DEFAULT_LINE_WIDTH * 3;
+              chart.update();
+            }
+          },
+          // Reset lines when leaving legend
+          onLeave: (evt) => {
+            let datasets = evt.chart.data.datasets;
+            for (let i = 0; i < datasets.length; i++) {
+              datasets[i].borderWidth = DEFAULT_LINE_WIDTH;
+            }
+            chart.update();
           }
-        ]
+        }
+      },
+      scales: {
+        x: {
+          type: "linear",
+        }
       },
       maintainAspectRatio: false
     }
   });
+
+
+  // Toggles all lines on/off
+  // If at least one line is hidden, show all lines
+  // If all lines are shown, hide all lines
+  let showHideAllLines = function() {
+    let anyIsHidden = false;
+
+    chart.data.datasets.forEach(function(ds, index) {
+      if (chart.getDatasetMeta(index).hidden)
+        anyIsHidden = true;
+    });
+
+    chart.data.datasets.forEach(function(ds, index) {
+      chart.getDatasetMeta(index).hidden=!anyIsHidden;
+    });
+    chart.update();
+  }
+
+  document.getElementById('toggle-all').addEventListener('click', showHideAllLines);
+
+  return chart;
 }
+
+$(function() {
+  var chartInitialized = false;
+
+  $('#toggle-chart').click(function() {
+    $('#chart-container').fadeToggle();
+    $('#chart-controls').fadeToggle();
+
+      if (!chartInitialized) {
+          let ctx = $('#chart').get(0).getContext('2d');
+
+          fetch('game.json')
+              .then(function(response) {
+                  return response.json();
+              })
+              .then(function(gameData) {
+                  chartInitialized = true;
+                  setupChart(ctx, gameData);
+              });
+      }
+  });
+});
